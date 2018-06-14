b6526df0c7e568c6c6ef91466eff8242f5d31c05
David Barkol <dbarkol@hotmail.com>
2018-06-13 22:30:02 -0700

    Added publish to custom topic

--- PhantomFuncApp/ContainerAFunc.cs
+++ PhantomFuncApp/ContainerAFunc.cs
@@ -1,23 +1,32 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGridExtensionConfig?functionName={functionname}
 
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Phantom.Models;
 
 namespace PhantomFuncApp
 {
     public static class ContainerAFunc
     {
         [FunctionName("ContainerAFunc")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, TraceWriter log)
+        public static async Task Run([EventGridTrigger]EventGridEvent eventGridEvent, TraceWriter log)
         {
             log.Info("ContainerAFunc triggered");
             log.Info(eventGridEvent.Data.ToString());
 
             // Send event to custom topic
-            // TODO: Implement
+            var fileInfo = new CustomFileInfo
+            {
+                Filename = eventGridEvent.Subject,
+                Available = true,
+                Description = "This is from containera"
+            };
+
+            await Utils.SendEvent(fileInfo, "Phantom.NewFile.FileTypeA", "subjecta");
         }
     }
 }
--- PhantomFuncApp/ContainerBFunc.cs
+++ PhantomFuncApp/ContainerBFunc.cs
@@ -1,24 +1,31 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGridExtensionConfig?functionName={functionname}
 
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Phantom.Models;
 
 namespace PhantomFuncApp
 {
     public static class ContainerBFunc
     {
         [FunctionName("ContainerBFunc")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, TraceWriter log)
+        public static async Task Run([EventGridTrigger]EventGridEvent eventGridEvent, TraceWriter log)
         {
             log.Info("ContainerBFunc triggered");
             log.Info(eventGridEvent.Data.ToString());
 
-            // Send event to custom topic
-            // TODO: Implement
+            var fileInfo = new CustomFileInfo
+            {
+                Filename = eventGridEvent.Subject,
+                Available = true,
+                Description = "This is from containerb"
+            };
 
+            await Utils.SendEvent(fileInfo, "Phantom.NewFile.FileTypeB", "subjectb");
         }
     }
 }
